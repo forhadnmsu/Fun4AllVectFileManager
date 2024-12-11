15962e02f5bfdaeb080534bd4894813e6cb2f6ea
Forhad <forhad16@nmsu.edu>
2024-12-11 12:48:37 -0600

    first commit

---
+++ convert/ConvertToVect.C
@@ -0,0 +1,29 @@
+#include <string>
+#include <iostream>
+R__LOAD_LIBRARY(libfun4all_vect_file_manager)
+R__LOAD_LIBRARY(libcalibrator)
+R__LOAD_LIBRARY(libktracker)
+
+int ConvertToVect(const char* fn_udst="/seaquest/users/mhossain/HitMatrix/e1039-analysis/SimChainDev/data/JPsi_vec_test/1/out/DST.root") {
+	Fun4AllServer* se = Fun4AllServer::instance();
+	Fun4AllInputManager *in = new Fun4AllDstInputManager("DUMMY");
+	se->registerInputManager(in);
+	in->fileopen(fn_udst);
+
+	/*
+	   Fun4AllVectEventInputManager* in = new Fun4AllVectEventInputManager("VectIn");
+	   in->Verbosity(100);
+	   in->enable_E1039_translation();
+	   in->set_tree_name("tree");
+	//	in->fileopen("/seaquest/users/mhossain/HitMatrix/e1039-analysis/SimChainDev/data/JPsi_vec_test/1/out/DST.root");
+	se->registerInputManager(in);
+	*/
+	Fun4AllVectEventOutputManager* tree = new Fun4AllVectEventOutputManager();
+	tree->Verbosity(100);
+	tree->SetTreeName("tree");
+	tree->SetFileName("test.root");
+	se->registerOutputManager(tree);
+	se->run();
+	se->End();
+	return 0;
+}
---
+++ work_reco/SimChainDev_Vect/Fun4Sim_reco_data2024.C
@@ -0,0 +1,94 @@
+R__LOAD_LIBRARY(libcalibrator)
+R__LOAD_LIBRARY(libktracker)
+R__LOAD_LIBRARY(libfun4all_vect_file_manager)
+
+int Fun4Sim_reco_data2024(const int n_evt = 10){
+  recoConsts* rc = recoConsts::instance();
+  //rc->set_IntFlag("RUNNUMBER", run_id);
+  rc->set_DoubleFlag("FMAGSTR", -1.044);
+  rc->set_DoubleFlag("KMAGSTR", -1.025);
+  rc->set_BoolFlag("COARSE_MODE", false);
+  rc->set_BoolFlag("REQUIRE_MUID", false);
+  rc->set_CharFlag("HIT_MASK_MODE", "X");
+    
+  rc->set_CharFlag("AlignmentMille", "config/align_mille_v10_a.txt");
+  rc->set_CharFlag("AlignmentHodo", "");
+  rc->set_CharFlag("AlignmentProp", "");
+  rc->set_CharFlag("Calibration", "");
+  rc->set_IntFlag ("MaxHitsDC0" , int(350)); // /3.0
+  rc->set_IntFlag ("MaxHitsDC1" , int(350)); // /3.0
+  rc->set_IntFlag ("MaxHitsDC2" , int(170)); // /3.0
+  rc->set_IntFlag ("MaxHitsDC3p", int(140)); // /3.0
+  rc->set_IntFlag ("MaxHitsDC3m", int(140)); // /3.0
+  rc->set_DoubleFlag("RejectWinDC0" , 0.3);
+  rc->set_DoubleFlag("RejectWinDC1" , 0.5);
+  rc->set_DoubleFlag("RejectWinDC2" , 0.35);
+  rc->set_DoubleFlag("RejectWinDC3p", 0.24);
+  rc->set_DoubleFlag("RejectWinDC3m", 0.24);
+  
+  Fun4AllServer* se = Fun4AllServer::instance();
+  //se->setRun(run_id);
+  //se->Verbosity(1);
+/*
+  CalibHitElementPos* cal_ele_pos = new CalibHitElementPos();
+  cal_ele_pos->CalibTriggerHit(false);
+  se->registerSubsystem(cal_ele_pos);
+  
+  CalibHodoInTime* cal_hodo = new CalibHodoInTime();
+  cal_hodo->SkipCalibration();
+  cal_hodo->DeleteOutTimeHit();
+  se->registerSubsystem(cal_hodo);
+  
+  CalibDriftDist* cal_dd = new CalibDriftDist();
+  cal_dd->Verbosity(999);
+  cal_dd->DeleteOutTimeHit();
+  se->registerSubsystem(cal_dd);
+*/
+  SQReco* reco = new SQReco();
+  //reco->Verbosity(99);
+  reco->set_legacy_rec_container(false); // default = true
+  reco->set_geom_file_name((string)gSystem->Getenv("E1039_RESOURCE") + "/geometry/geom_run005433.root");
+  reco->set_enable_KF(true);
+  reco->setInputTy(SQReco::E1039);
+  reco->setFitterTy(SQReco::KFREF);
+  reco->set_evt_reducer_opt("none");
+  reco->set_enable_eval_dst(true);
+  for (int ii = 0; ii <= 3; ii++) reco->add_eval_list(ii);
+  reco->set_enable_eval(true);
+  reco->set_eval_file_name("eval.root");
+  se->registerSubsystem(reco);
+
+  //VertexFit* vtx_fit = new VertexFit();
+  ////vtx_fit->set_eval_file_name(vtxevalloc);
+  //se->registerSubsystem(vtx_fit);
+
+  SQVertexing* vtx = new SQVertexing();
+  vtx->Verbosity(99);
+  //vtx->set_legacy_rec_container(true); // default = false
+  //vtx->set_single_retracking(true);
+  se->registerSubsystem(vtx);
+
+
+
+ Fun4AllVectEventInputManager* in = new Fun4AllVectEventInputManager("VectIn");
+ in->Verbosity(100);
+ in->enable_E1039_translation();
+ in->set_tree_name("tree");
+ in->fileopen("../../convert/test.root");
+ se->registerInputManager(in);
+ ///////////////////////////////////////////
+  // Output
+  ///////////////////////////////////////////
+
+  // DST output manager
+  Fun4AllDstOutputManager *out = new Fun4AllDstOutputManager("DSTOUT", "DST.root");
+  se->registerOutputManager(out);
+
+
+
+  se->run(n_evt);
+  se->End();
+  //rc->WriteToFile("recoConsts.tsv");
+  delete se;
+  exit(0); //return 0;
+}
